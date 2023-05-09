65227d56c76bfe78c0ef8b27a3cc06c6b390f972
Samarth <bhats765@newschool.edu>
2023-05-09 00:09:50 -0400

    js check

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 const boxes = document.getElementById('cls-7');
 const rect_data = document.getElementsByClassName('rectangle');
 
-fetch('./edit.json')
-
 boxes.addEventListener("click", dataDisplay);
 
-
+const zipId = 10012;
 
 function dataDisplay () {
   console.log("bhai chal gaya");
@@ -16,4 +14,24 @@ function dataDisplay () {
  } else {
   rect_data[0].style.display = "none";
  }
+
+
+ console.log(zipId);
+
+ var addresses = [];
+
+fetch("https://raw.githubusercontent.com/Samarthbhatia33/final_manhattan/main/edit.json")
+ .then((response) => response.json()) 
+ .then((data) => {
+   
+   
+   for(let i = 0; i < data.length; i ++){
+    if (data[i].ZIP_CODE === zipId) {
+      addresses.push(data[i].INCIDENT_ADDRESS);
+      console.log(addresses);
+
+    }
+ }
+});
+
 }
